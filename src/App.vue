fd744c7a6d1da1713a7294c1d2b50227cf264abc
guishijin <420188751@qq.com>
2018-08-07 13:31:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Hello></Hello>
+    <hr />
+    <calculator></calculator>
+    <calculator></calculator>
+    <calculator></calculator>
+    <calculator></calculator>
+    <hr />
+    <br />
+    <HelloWorld></HelloWorld>
+    <hr />
+    <br />
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import HelloWorld from './components/HelloWorld'
+import calculator from './components/calculator'
+
+export default {
+  name: 'App',
+  components: {
+    Hello,
+    HelloWorld,
+    calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
